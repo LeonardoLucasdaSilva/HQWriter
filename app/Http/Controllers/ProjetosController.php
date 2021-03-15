f3e815ab9039aaeb375a2036e99edc9790b8b2d3
LeonardoLucasdaSilva <leonardo3443@gmail.con>
2021-03-15 19:09:27 -0300

    teste

--- app/Http/Controllers/ProjetosController.php
+++ app/Http/Controllers/ProjetosController.php
@@ -378,6 +378,7 @@ class ProjetosController extends Controller
 
     public function visualizarRoteiro($roteiro)
     {
+        dd($roteiro);
         $roteiro = Roteiro::where('id',$roteiro)->first();
         $paginas = Pagina::where('roteiro_id', $roteiro->id)->get();
         $queryautor = User::where('id', $roteiro->user_id)->get();
