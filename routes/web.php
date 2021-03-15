3df0eef36051784079aee7b9cb206dcdb9f18484
LeonardoLucasdaSilva <leonardo3443@gmail.con>
2021-03-15 18:03:07 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if(Auth::check()) {
-        return redirect()->route('projetos.index');
+        return view('home');
     }
     else{
         return view('welcome');
